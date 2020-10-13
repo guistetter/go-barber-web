00fe4d8d063c755d95e5cc5de376338df3085b9d
guistetter <guilherme_stetter@hotmail.com>
2020-10-13 14:41:42 -0300

    create switch render layouts

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,19 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate = false,
   ...rest
 }) {
   const signed = false;
+
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
+
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
-  return <Route {...rest} component={Component} />;
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
@@ -22,6 +39,7 @@ RouteWrapper.propTypes = {
   component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
     .isRequired,
 };
+
 RouteWrapper.defaultProps = {
   isPrivate: false,
 };
