72090412bfdd86303353594733f9e2391cb2d26b
guistetter <guilherme_stetter@hotmail.com>
2020-10-13 15:54:51 -0300

    create form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
 
 export default function SignIn() {
-  return <div>Ola SignIn</div>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form action="">
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="passwrod" placeholder="Sua senha secreta" />
+        <button type="submit">Acessar</button>
+        <Link to="/register"> Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
