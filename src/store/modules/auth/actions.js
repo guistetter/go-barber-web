76336ae6459a84b5130e5a179a795802f1c0398a
guistetter <guilherme_stetter@hotmail.com>
2020-10-14 17:37:35 -0300

    refactor name function

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,11 +1,11 @@
-export function SignInRequest(email, password) {
+export function signInRequest(email, password) {
   return {
     type: '@auth/SIGN_IN_REQUEST',
     payload: { email, password },
   };
 }
 
-export function SigninSuccess(token, user) {
+export function signInSuccess(token, user) {
   return {
     type: '@auth/SIGN_IN_SUCCESS',
     payload: { token, user },
