7802c0881f08de6f7ae459f322f30b383f6052f6
guistetter <guilherme_stetter@hotmail.com>
2020-10-15 14:05:34 -0300

    create dashboard

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../../assets/logo2.svg'
+import {Container, Content, Profile} from './styles'
+
+export default function Header(){
+  return(
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBaber"/>
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>GUI STETTER</strong>
+              <Link to="/profile">Meu Perfil</Link>
+              </div>
+              <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="Foto do usuario"/>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+
+  )
+}
