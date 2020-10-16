742312732f113304a6ab6b4a31f8934f295e4ba8
guistetter <guilherme_stetter@hotmail.com>
2020-10-16 14:44:23 -0300

    create dispatch signout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import {Form, Input} from '@rocketseat/unform'
+import { signOut } from '../../store/modules/auth/actions'
 import {Container} from './styles'
 import {updateProfileRequest} from '../../store/modules/user/actions'
 import AvatarInput from './AvatarInput'
@@ -10,6 +11,10 @@ export default function Profile() {
 
   function handleSubmit(data){
     dispatch(updateProfileRequest(data))
+  }
+
+  function handleSignOut(){
+    dispatch(signOut())
   }
 
   return (
@@ -24,7 +29,7 @@ export default function Profile() {
         <Input type="password" name="confirmPassword" placeholder="Confirme sua nova senha"/>
         <button type="submit">Atualizar Perfil</button>
       </Form>
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>Sair do GoBarber</button>
     </Container>
   )
 }
