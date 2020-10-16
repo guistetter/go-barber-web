5c2f8ca3f4ab5a3eeee7770dd73491ff3405269b
guistetter <guilherme_stetter@hotmail.com>
2020-10-16 14:05:34 -0300

    add input Avatar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,7 +3,7 @@ import {useDispatch, useSelector} from 'react-redux'
 import {Form, Input} from '@rocketseat/unform'
 import {Container} from './styles'
 import {updateProfileRequest} from '../../store/modules/user/actions'
-
+import AvatarInput from './AvatarInput'
 export default function Profile() {
   const dispatch = useDispatch()
   const profile = useSelector(state => state.user.profile)
@@ -15,6 +15,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id"/>
         <Input name="name" placeholder="Nome Completo"/>
         <Input name="email" placeholder="Seu endereço de e-mail"/>
         <hr/>
