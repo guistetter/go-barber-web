fd049b8d339b4fceeab7484d009996773bee0fc4
guistetter <guilherme_stetter@hotmail.com>
2020-10-16 15:14:03 -0300

    createDashBoard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import api from '../../services/api'
+
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+
+import { Container, Time } from './styles'
 
 export default function DashBoard() {
-  return <div>Ola DashBoard</div>;
+  api.get('appointments')
+
+  return (
+    <Container>
+      <header>
+      <button>
+        <MdChevronLeft size={36} color="#FFF"/>
+      </button>
+      <strong>31 de Maio</strong>
+      <button>
+        <MdChevronRight size={36} color="#FFF"/>
+      </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00 </strong>
+          <span> Gui </span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span> Em aberto </span>
+        </Time>
+        <Time>
+          <strong>10:00 </strong>
+          <span> Gui </span>
+        </Time>
+        <Time>
+          <strong>10:00 </strong>
+          <span> Gui </span>
+        </Time>
+      </ul>
+    </Container>
+  )
 }
