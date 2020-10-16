0e088bd3bd69bb9b71dcfd9fdd98964c17442e2d
guistetter <guilherme_stetter@hotmail.com>
2020-10-16 11:19:08 -0300

    createa profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import {Form, Input} from '@rocketseat/unform'
+
+import {Container} from './styles'
 
 export default function Profile() {
-  return <div>Ola Profile</div>;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome Completo"/>
+        <Input name="email" placeholder="Seu endereço de e-mail"/>
+        <hr/>
+        <Input type="password" name="oldPassword" placeholder="Sua senha antiga"/>
+        <Input type="password" name="password" placeholder="Sua nova senha"/>
+        <Input type="password" name="confirmPassword" placeholder="Confirme sua nova senha"/>
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  )
 }
